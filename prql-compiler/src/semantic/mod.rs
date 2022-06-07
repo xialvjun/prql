7b6967db158f381812f33454359cfb582a23ade9
aljazerzen <aljaz.erzen@gmail.com>
2022-06-07 22:08:36 +0200

    type resolver WIP

--- prql-compiler/src/semantic/mod.rs
+++ prql-compiler/src/semantic/mod.rs
@@ -6,6 +6,9 @@ mod name_resolver;
 mod reporting;
 mod scope;
 mod transforms;
+mod type_resolver;
+
+use crate::ast::Node;
 
 pub use self::context::Context;
 pub use self::declarations::{Declaration, Declarations};
@@ -13,3 +16,11 @@ pub use self::frame::{Frame, FrameColumn};
 pub use self::scope::{split_var_name, Scope};
 pub use name_resolver::resolve_names;
 pub use reporting::{collect_frames, label_references};
+pub use type_resolver::resolve_types;
+
+/// Runs semantic analysis on the query, using current state.
+///
+/// Note that this removes function declarations from AST and saves them as current context.
+pub fn resolve(nodes: Vec<Node>, context: Option<Context>) -> anyhow::Result<(Vec<Node>, Context)> {
+    resolve_names(nodes, context).and_then(|(n, c)| resolve_types(n, c))
+}
